13f07cbd2a7718524efe1ec0e48d0e708cdb8947
Victor FG <v.fernandez@iskra.cat>
2020-12-23 16:33:30 +0100

    project init commit

--- App.js
+++ App.js
@@ -2,10 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {
+  createAppContainer,
+  createSwitchNavigator
+} from 'react-navigation'
+
+import SigninScreen from './src/screens/SigninScreen';
+import SignupScreen from './src/screens/SignupScreen';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola Buti, sóc l'emulador d'Android!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ README.md
@@ -0,0 +1,12 @@
+- Build with command "npx expo-cli init".
+
+To run your project, navigate to the directory and run one of the following npm commands.
+
+- cd rolGameApp
+- npm start # you can open iOS, Android, or web from here, or run them directly with the commands below.
+- npm run android
+- npm run ios
+- npm run web
+
+
+- expo r -c  Start the app and clear cache with
